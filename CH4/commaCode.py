a0b632ee8977c83fec93d9c230805d4feb993d07
zayazt <132500291+zayazt@users.noreply.github.com>
2025-01-09 08:29:43 -0600

    evening work

---
+++ CH4/commaCode.py
@@ -0,0 +1,7 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+print(str(spam))
+
+# def shopList(scooby):
+        # print(str(scooby[0:4]))
+
+# shopList(spam)
--- CH4/conway.py
+++ CH4/conway.py
@@ -26,4 +26,42 @@ while True: # Main program loop.
 
     # Calculate the next step's cells based on current step's cells:
     for x in range(WIDTH):
-        for y in range
+        for y in range(HEIGHT):
+            # Get nieghboring coordinates:
+            # '% WIDTH' ensures leftCoord is always between 0 and WIDTH -1
+            leftCoord = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+            
+            # Count number of living neighbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors += 1 # Top neighbor is alive.
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-right neighbor is alive.
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors += 1 # Left neighbor is alive.
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors += 1 # Right neighbor is alive.
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-left neighbor is alive.
+            if currentCells[x][belowCoord] == '#':
+                numNeighbors += 1 # Bottom neighbor is alive.
+            if currentCells[rightCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-right neighbor is alive.
+
+            # Set cell based on Conway's Game of Life rules:
+            if currentCells[x][y] == '#' and (numNeighbors == 2 or
+numNeighbors == 3):
+                # Living cells with 2 or 3 neighbors stay alive:
+                nextCells[x][y] = '#'
+            elif currentCells[x][y] == ' ' and numNeighbors == 3:
+                # Dead cells with 3 neighbors become alive:
+                nextCells[x][y] = '#'
+            else:
+                # Everything else dies or stays dead:
+                nextCells[x][y] = ' '
+    time.sleep(1) # Add a 1-second pause to reduce flickering.
